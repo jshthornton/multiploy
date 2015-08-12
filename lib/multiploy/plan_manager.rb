348b28caa0daefe6cb973b6c23ceedc4b0077824
Joshua Thornton <joshua@techinasia.com>
2015-08-12 14:08:30 +0800

    Method of resolving factory or raw classes for plans.

--- lib/multiploy/plan_manager.rb
+++ lib/multiploy/plan_manager.rb
@@ -8,5 +8,19 @@ module Multiploy
     def summon_class(namespace)
       Object.const_get namespace
     end
+
+    def resolve_factory(klass)
+      # Class method
+      return klass.make if klass.respond_to? :make
+
+      # Factory instance method
+      if klass.method_defined? :make then
+        factory = klass.new
+        return factory.make
+      end
+
+      # Raw
+      return klass.new
+    end
   end
 end
--- spec/multiploy/plan_manager_spec.rb
+++ spec/multiploy/plan_manager_spec.rb
@@ -26,4 +26,45 @@ describe Multiploy::PlanManager do
       expect(Klass).to eq(Multiploy::PlanManager)
     end
   end
+
+  describe '#resolve_factory' do
+    context 'with class method' do
+      let(:klass) do
+        Class.new do
+          def self.make
+            'class'
+          end
+        end
+      end
+
+      it 'returns result from class method' do
+        expect(subject.resolve_factory(klass)).to eq('class')
+      end
+    end
+
+    context 'with factory instance' do
+      let(:klass) do
+        Class.new do
+          def make
+            'factory'
+          end
+        end
+      end
+
+      it 'returns result from factory' do
+        expect(subject.resolve_factory(klass)).to eq('factory')
+      end
+    end
+
+    context 'with raw' do
+      let(:klass) do
+        Class.new do
+        end
+      end
+
+      it 'returns raw class' do
+        expect(subject.resolve_factory(klass)).to be_an_instance_of(klass)
+      end
+    end
+  end
 end
