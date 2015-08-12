d712952e1d4f24f2ec9712759412e36cdd80790b
Joshua Thornton <joshua@techinasia.com>
2015-08-12 10:28:52 +0800

    Spec for #summon_class on PlanManager.

--- spec/multiploy/plan_manager_spec.rb
+++ spec/multiploy/plan_manager_spec.rb
@@ -13,6 +13,17 @@ describe Multiploy::PlanManager do
   end
 
   describe '#summon_class' do
+    it 'calls lower-level' do
+      allow(Object).to receive(:const_get)
+      expect(Object).to receive(:const_get).once.with('hello')
 
+      subject.summon_class('hello')
+    end
+
+    it 'returns class' do
+      Klass = subject.summon_class('Multiploy::PlanManager')
+
+      expect(Klass).to eq(Multiploy::PlanManager)
+    end
   end
 end
