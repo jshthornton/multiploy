a8e1cee29858544be81302c5c0176779e7cb1a91
Joshua Thornton <joshua@techinasia.com>
2015-08-13 11:22:09 +0800

    Cleaner CLI interface with bin.

---
+++ lib/multiploy/cli.rb
@@ -0,0 +1,74 @@
+require 'optparse'
+require 'ostruct'
+require_relative 'version'
+
+module Multiploy
+  #
+  class CLI
+    def initialize(command = nil)
+      @options = OpenStruct.new
+      @command = command
+      assign_defaults
+    end
+
+    def assign_defaults
+      @options.verbose = false
+    end
+
+    def self.parse_core(options, args, parser)
+      parser.banner = 'Usage: bin.rb [options]'
+
+      parser.separator ''
+      parser.separator 'Specific options:'
+
+      # # Queue ID
+      # parser.on('-q', '--queue-id QUEUEID',
+      #           'The queue id to send a message to') do |id|
+      #   options.queue_id = id
+      # end
+
+      # # Action
+      # parser.on('-a', '--action ACTION',
+      #           'The action key') do |action|
+      #   options.action = action
+      # end
+
+      # # Arguments
+      # parser.on('--arguments x,y,z', Array, '') do |arguments|
+      #   options.arguments = arguments
+      # end
+
+      parser.on('-v', '--[no-]verbose', 'Run verbosely') do |v|
+        options.verbose = v
+      end
+
+      parser.separator ''
+      parser.separator 'Common options:'
+
+      # No argument, shows at tail.  This will print an options summary.
+      # Try it and see!
+      parser.on_tail('-h', '--help', 'Show this message') do
+        puts opts
+        exit
+      end
+
+      # Another typical switch to print the version.
+      parser.on_tail('--version', 'Show version') do
+        puts Multiploy::Version
+        exit
+      end
+
+      parser.parse!(args)
+
+      options
+    end
+
+    def parse(args)
+      self.class.parse_core @options, args, OptionParser.new
+    end
+
+    def run
+      @command.execute
+    end
+  end
+end
