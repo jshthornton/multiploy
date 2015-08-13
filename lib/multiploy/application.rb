dbae572cd9df6f033a75f2d699101c4435574d13
Joshua Thornton <joshua@techinasia.com>
2015-08-13 14:44:23 +0800

    Application now resolves plan from config options.

--- lib/multiploy/application.rb
+++ lib/multiploy/application.rb
@@ -1,17 +1,28 @@
-require_relative './actions/fetch/remote'
-require_relative './actions/store/file'
-require_relative './actions/transfer/sshkit_file'
-require_relative './actions/deploy/default'
+require_relative 'configuration_helper'
+require_relative 'plan_manager'
+require 'configuration'
 
 #
 module Multiploy
   #
   class Application
+
+    def initialize
+      @configuration_helper = ConfigurationHelper.new
+      @plan_manager = PlanManager.new
+    end
+
     def execute
-      fetcher = Multiploy::Fetch::Remote.new
-      storer = Multiploy::Store::File.new
-      transferer = Multiploy::Transfer::SSHKitFile.new
-      deployer = Multiploy::Deploy::Default.new
+      @configuration_helper.load_default_config
+      @configuration_helper.load_user_config
+      @configuration_helper.merge_config
+
+      core_config = Configuration.for 'core'
+      @plan_manager.load core_config.plan.file_path
+      plan_klass = @plan_manager.summon_class core_config.plan.class_lookup
+      @plan = @plan_manager.resolve_factory plan_klass
+
+      @plan.execute
     end
   end
 end
