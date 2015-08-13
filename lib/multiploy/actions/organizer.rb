846c73d5df7a28561379aa5a512c52d1eff02b7b
Joshua Thornton <joshua@techinasia.com>
2015-08-13 13:55:00 +0800

    Fixed organizer not actually have its array setup.

--- lib/multiploy/actions/organizer.rb
+++ lib/multiploy/actions/organizer.rb
@@ -3,6 +3,10 @@ module Multiploy
   class Organizer
     attr_accessor :steps
 
+    def initialize
+      @steps = []
+    end
+
     def execute
       @steps.each { |step| step.execute }
     end
