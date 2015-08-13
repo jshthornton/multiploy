dd5658af922e55c4dee355bd57f80b241031db3f
Joshua Thornton <joshua@techinasia.com>
2015-08-13 11:23:13 +0800

    Basic deploy action specs.

--- spec/multiploy/actions/deploy/default_spec.rb
+++ spec/multiploy/actions/deploy/default_spec.rb
@@ -1,5 +1,19 @@
 require_relative '../../../../lib/multiploy/actions/deploy/default'
 
+describe Multiploy::Deploy::ActionFactory do
+  subject { described_class.new }
+
+  describe '#makeSetup' do
+  end
+
+  describe '#makeInstall' do
+
+  end
+
+  describe '#makeDeploy' do
+
+  end
+end
 
 describe Multiploy::Deploy::DateTimeNaming do
   subject { described_class.new }
@@ -26,5 +40,68 @@ describe Multiploy::Deploy::DateTimeNaming do
     end
   end
 end
+
+describe Multiploy::Deploy::SetupDirectory do
+  subject { described_class.new }
+
+  describe '#execute' do
+    it 'calls each backend' do
+      coordinator = double('SSHKit::Coordinator')
+
+      expect(coordinator).to receive(:each)
+
+      subject.coordinator = coordinator
+      subject.execute
+    end
+  end
+
+  describe '#backend_action' do
+    it 'calls execute for backend' do
+      backend = Object.new
+      backend.define_singleton_method(:execute!) do |*args|
+      end
+
+      subject.path = 'hello'
+
+      allow(backend).to receive(:instance_exec).and_call_original
+      allow(backend).to receive(:execute!)
+
+      expect(backend).to receive(:execute!).with(:mkdir, '-p', 'hello')
+
+      backend.instance_exec({}, &subject.backend_action)
+    end
+  end
+end
+
+describe Multiploy::Deploy::MoveToDestination do
+  subject { described_class.new }
+
+  describe '#execute' do
+    it 'calls each backend' do
+      coordinator = double('SSHKit::Coordinator')
+
+      expect(coordinator).to receive(:each)
+
+      subject.coordinator = coordinator
+      subject.execute
+    end
+  end
+
+  describe '#backend_action' do
+    it 'calls execute for backend' do
+      backend = Object.new
+      backend.define_singleton_method(:execute!) do |*args|
+      end
+
+      subject.src_path = 'hello'
+      subject.dest_path = 'world'
+
+      allow(backend).to receive(:instance_exec).and_call_original
+      allow(backend).to receive(:execute!)
+
+      expect(backend).to receive(:execute!).with(:tar, '-zxvf', 'hello', '-C', 'world', '--strip')
+
+      backend.instance_exec({}, &subject.backend_action)
+    end
   end
 end
